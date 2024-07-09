5c4b5b8a1dc79f12740e46a44265c2f214c37844
Hüseyin Eren <143540629+huseyinfest@users.noreply.github.com>
2024-07-09 12:21:23 +0300

    Kodlar
    Arduino programı için hazırlanmış kodlar

---
+++ TelegramSicaklik/TelegramSicaklik.ino
@@ -0,0 +1,68 @@
+// Bu proje Deneyap Kart'ın dahili IMU sensörü kullanılarak yapılmıştır.
+#include <DeneyapTelegram.h>
+#include <lsm6dsm.h>
+
+#define WIFI_SSID "Eren"        // Wifi Adı
+#define WIFI_PASS "eren723134"  // Wifi Şifresi
+
+int a = 0;
+int b = 0;
+String ID = "5632619913";     // Telegram @myidbot botundan aldığınız ID numaranız
+String begin = "7061206556:AAH_FzBHQkwv3KC-I3oqXIIr0P9RNBmf2Ds"; // Telegram botunuzun API Adresi
+float sicaklik;
+unsigned long timer1 = 0;
+unsigned long timer2 = 0;
+
+DeneyapTelegram telegram;
+LSM6DSM IMU;
+void setup() {
+  Serial.begin(115200);
+  IMU.begin();
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Wi-Fi Baglanıyor...");
+  while (WiFi.status() != WL_CONNECTED){
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println("Baglandi");
+  telegram.begin(begin); // Telegram Bot Tokeniniz
+  telegram.send(ID, "DK Telegrama bağlandı."); // Kanal ID ve Mesaj
+  sicaklik = IMU.readTempC();
+  sicaklik = round(sicaklik * 100) / 100;
+  telegram.send(ID, "Sıcaklık: " + String(sicaklik) + " C°");
+}
+
+void loop() {
+  sicaklik = IMU.readTempC();
+  sicaklik = round(sicaklik * 100) / 100;
+  if (millis() - timer1 >= 1000) {   // a değişkeni saniye olarak atanmıştır.
+    a++;
+    Serial.print("Sıcaklık: " + String(sicaklik));
+    Serial.println("\t Saniye: " + String(a));
+    timer1 = millis();
+  }
+  if (a == 3) {  // Saniye 300(5 dk) olduğunda telegrama veri gönderir projenize göre değiştirebilirsiniz.
+    telegram.send(ID, "Sıcaklık: " + String(sicaklik) + " C°");
+    a = 0;
+  }
+  if (sicaklik >= 35) {
+    if (millis() - timer2 >= 1000) {
+      b++;
+      timer2 = millis();
+    }
+    if (b == 3) { // sıcaklık 35 derecenin üstüne çıktığında 60 saniyede bir veri gönderir.
+      telegram.send(ID, "Sıcaklık: " + String(sicaklik) + " C° yükseldi.");
+      b = 0;
+    }
+  }
+  if (sicaklik <= 25) {
+    if (millis() - timer2 >= 1000) {
+      b++;
+      timer2 = millis();
+    }
+    if (b == 3) { // sıcaklık 25 derecenin altına düştüğünde 60 saniyede bir veri gönderir.
+      telegram.send(ID, "Sıcaklık: " + String(sicaklik) + " C° düştü.");
+      b = 0;
+    }
+  }
+}
